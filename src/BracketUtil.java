20c2a73b9bfff671bcf962a6898696e1f151a20f
Ilyas <i.ismailov@dengiclick.kz>
2022-07-18 16:22:38 +0600

    OTUS-JB-trainer-9: completed task with description

---
+++ src/BracketUtil.java
@@ -0,0 +1,37 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class BracketUtil {
+    public static boolean isCorrectBrackets(String input,
+                                            char bracketOpenSymbol,
+                                            char bracketCloseSymbol) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = 0; i < input.length(); i++)
+        {
+            char charInput = input.charAt(i);
+            if (charInput == bracketOpenSymbol)
+            {
+                stack.push(charInput);
+                continue;
+            }
+
+            if (stack.isEmpty()) return false;
+
+            if (charInput == bracketCloseSymbol){
+                stack.pop();
+            }
+        }
+        return (stack.isEmpty());
+    }
+
+    private BracketUtil() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ввод равного поличества открывающих и закрывающих скобок - "
+                + BracketUtil.isCorrectBrackets("{{}}", '{', '}'));
+        System.out.println("Ввод неравного поличества открывающих и закрывающих скобок - "
+                + BracketUtil.isCorrectBrackets("{{}}}", '{', '}'));
+    }
+}
