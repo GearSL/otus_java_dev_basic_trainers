259130ca333e1d5059e0247247d28f6770b7ba4d
ilyas <i.ismailov@dengiclick.kz>
2022-08-02 20:51:42 +0600

    OTUS-JB-trainer-10: Fix bug for fibo1

--- src/kz/ibr/trainers/trainer10/FibonacciUtils.java
+++ src/kz/ibr/trainers/trainer10/FibonacciUtils.java
@@ -6,7 +6,7 @@ public class FibonacciUtils {
         int fib2 = 1;
         int fibN;
         int result=fib1 + fib2;
-        if(limit < 1){
+        if(limit <= 1){
             return "0";
         }
 
@@ -26,7 +26,10 @@ public class FibonacciUtils {
     }
 
     public static void main(String[] args) {
+        System.out.println("Сумма чисел фибоначчи до 0 должна равняться 0 - " + FibonacciUtils.getFibonacciSum((byte) 0));
+        System.out.println("Сумма чисел фибоначчи до 1 должна равняться 0 - " + FibonacciUtils.getFibonacciSum((byte) 1));
         System.out.println("Сумма чисел фибоначчи до 2 должна равняться 1 - " + FibonacciUtils.getFibonacciSum((byte) 2));
+        System.out.println("Сумма чисел фибоначчи до 3 должна равняться 2 - " + FibonacciUtils.getFibonacciSum((byte) 3));
         System.out.println("Сумма чисел фибоначчи до 4 должна равняться 4 - " + FibonacciUtils.getFibonacciSum((byte) 4));
         System.out.println("Сумма чисел фибоначчи до 5 должна равняться 7 - " + FibonacciUtils.getFibonacciSum((byte) 5));
         System.out.println("Сумма чисел фибоначчи до 6 должна равняться 12 - " + FibonacciUtils.getFibonacciSum((byte) 6));
